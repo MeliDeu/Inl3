af5133da9557717eac25e26e54e2dd2cae19b90c
MeliDeu <m.deuretzbacher@yahoo.se>
2020-11-28 15:33:52 +0100

    update

---
+++ kod/api/dbhandler.php
@@ -0,0 +1,14 @@
+<?php error_reporting(-1);?>
+// id, name, password
+// id, name, creatorID, date, colors
+<?php
+    $database = "database.json";
+    $base = [
+        "users" => [
+            ["id" => 1, "name" => "melanie", "password" => "12345"],
+            ["id" => 2, "name" => "lolle", "password" => "12345"],
+            ["id" => 3, "name" => "erbarnicus", "password" => "12345"]
+        ],
+        "palettes" => []
+    ];
+?>
