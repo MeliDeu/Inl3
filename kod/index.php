61c297fb36fa9f31a8da7cf416cac02c2dabcfab
MeliDeu <m.deuretzbacher@yahoo.se>
2020-11-28 15:08:18 +0100

    First push

---
+++ kod/index.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Palettes</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon shortcut" href="assetts/palette.png">
+    </head>
+    <body>
+        
+    </body>
+</html>
---
+++ useThisIfYouWant.js
@@ -0,0 +1,21 @@
+
+function formatDate(date){
+    // Accepts a Date-object.
+    // Returns a date as a string with the format YY.MM.DD
+    return date.getFullYear().toString().substr(-2) + "." + (date.getMonth() + 1) + "." + date.getDate() ;
+}
+function getNowAsISO(){
+    // Returns the current time and date as a string with ISO format
+    // The return from this function can be saved in a DB as a string.
+    return new Date().toISOString();
+}
+function getFormattedDateFromText(text){
+    // Accepts a date+time as a string (for instance in ISO format)
+    // Returns the same date+time as received but in the format YY.MM.DD
+    // This function can be used to transform the string from DB into a string with the format YY.MM.DD
+    return formatDate(new Date(text));
+}
+
+let now = getNowAsISO();
+console.log("Current Date & Time (ISO format): " + now);
+console.log("Current Date & Time (YY.MM.DD): " + getFormattedDateFromText(now));
